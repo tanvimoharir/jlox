6a7261a76f382e3cedbe417bfc3264ed6f7937d5
tanvimoharir <tanvimoharir@gmail.com>
2021-07-11 00:28:22 +0530

    More changes,still non-working

--- lox/src/com/craftinginterpreters/lox/Lox.java
+++ lox/src/com/craftinginterpreters/lox/Lox.java
@@ -8,9 +8,13 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class Lox {
 
-	public static void main(String[] args) {
+
+public class Lox {
+	
+	static boolean hadError = false;
+
+	public static void main(String[] args) throws IOException {
 		
 		if (args.length > 1 ) {
 			System.out.println("Usage jlox [script]");
@@ -21,6 +25,47 @@ public class Lox {
 			runPrompt();
 		}
 
+	}
+
+	private static void runFile(String path) throws IOException {
+		byte[] bytes = Files.readAllBytes(Paths.get(path));
+		run(new String(bytes, Charset.defaultCharset()));
+		
+		if (hadError) System.exit(65);
+		
+	}
+
+	private static void runPrompt() throws IOException {
+		InputStreamReader input = new InputStreamReader(System.in);
+		BufferedReader reader = new BufferedReader(input);
+		
+		for(;;) {
+			System.out.println("> ");
+			String line = reader.readLine();
+			if (line == null) break;
+			run(line);
+			hadError = false;
+		}
+		
+	}
+
+	private static void run(String source) {
+		Scanner scanner = new Scanner(source);
+		List<Token> tokens = scanner.scanTokens();
+		
+		// For now just printing the tokens
+		for(Token token : tokens) {
+			System.out.println(token);
+		}
+	}
+	
+	static void error(int line, String message) {
+		report(line, "", message);
+	}
+	
+	private static void report(int line, String where, String message) {
+		System.err.println("[line " + line + "] Error" + where + ": " + message);
+		hadError = true;
 	}
 
 }
